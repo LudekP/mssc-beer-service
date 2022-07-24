eaa831a7cc11920ed84ed7f603fd3024f96ea0b9
BALLNAZZAR-PC\Ballnazzar <ludek.p@email.cz>
2022-07-24 10:40:58 +0200

    Add data model

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,39 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-05-12.
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto implements Serializable  {
+
+    static final long serialVersionUID = -5815566940065181210L;
+
+    private UUID id;
+    private Integer version;
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    private OffsetDateTime createdDate;
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private String upc;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private BigDecimal price;
+    private Integer quantityOnHand;
+
+}

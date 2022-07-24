f68ab0d1053183462ffffa1102e2aa06d1f3cef9
BALLNAZZAR-PC\Ballnazzar <ludek.p@email.cz>
2022-07-24 11:00:50 +0200

    Spring mvc rest services

---
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerServiceImpl.java
@@ -0,0 +1,37 @@
+package guru.springframework.msscbeerservice.services;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerPagedList;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BeerServiceImpl implements BeerService {
+    @Override
+    public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
+        return null;
+    }
+
+    @Override
+    public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
+        return null;
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return null;
+    }
+
+    @Override
+    public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
+        return null;
+    }
+
+    @Override
+    public BeerDto getByUpc(String upc) {
+        return null;
+    }
+}
